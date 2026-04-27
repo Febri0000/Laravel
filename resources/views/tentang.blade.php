bf667d6da113d9f063dce9f33984f7913c118968
febri <abangputraaa09@gmail.com>
2026-04-27 13:58:18 +0700

    Initial commit

---
+++ resources/views/tentang.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+    <h1>HALAMAN FERBI</h1>
+    <p>Ini Halaman 
+        Tentang Febri
+    </p>
+    
+    
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\FakultasController;
+use App\Http\Controllers\PeriodeController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+route ::get('/tentang', function(){
+    return view('tentang');
+});
+
+Route::resource('/fakultas', FakultasController::class);
+Route::resource('periodes', PeriodeController::class);
